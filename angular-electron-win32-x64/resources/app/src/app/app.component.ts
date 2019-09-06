f56a4edb23036d13b1b11ad0054127e4f09de626
Natan <natanlaraferreira@gmail.com>
2019-09-06 09:42:18 -0300

    Teste com versao e build para Windows

---
+++ angular-electron-win32-x64/resources/app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-electron';
+
+  constructor(){
+    //localStorage.setItem("versao", "123");
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { version } from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-electron';
+
+  constructor(){
+    //localStorage.setItem("versao", "123");
+    console.log(version);
+  }
 }
