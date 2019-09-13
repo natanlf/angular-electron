d1ad636cebdd32b66d71ade59de89ea6a1128bef
Natan <natanlaraferreira@gmail.com>
2019-09-13 17:21:59 -0300

    Gerando instalador com o codigo de atualizacao
    Gerar os arquivos de build conforme o comando.
    Verificar os nomes e diretórios.
    O --asar no fim do comando foi o que resolvei o problema de nome excendendo a quantidade de caracteres.
    
    electron-packager C:\ws\angular-electron --platform=win32 --arch=x64 angular-electron-built --asar
    
    Após gerar a pasta com os arquivos podemos gerar o instalador com o comando:
    node build.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, autoUpdater, dialog } = require('electron')
+require('update-electron-app')()
 
 let win;
 
@@ -119,3 +120,33 @@ function handleSquirrelEvent(application) {
 };
 
 ////////////////////////////////////////////////////////////////////
+
+
+const server = 'https://update.electronjs.org'
+const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+
+autoUpdater.setFeedURL(feed)
+
+
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 60000)
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+
+  dialog.showMessageBox(dialogOpts, (response) => {
+    if (response === 0) autoUpdater.quitAndInstall()
+  })
+})
+
+autoUpdater.on('error', message => {
+  console.error('Erro ao atualizar o aplicativo')
+  console.error(message)
+})
